d09a09f811f76540134f28109091bccfa0d8b881
Denny Kunker <wkunker@gmail.com>
2014-01-01 21:11:28 -0500

    Dropping and picking up items

--- main.py
+++ main.py
@@ -463,6 +463,294 @@ class Model(object):
         while self.queue:
             self._dequeue()
 
+
+
+class WorldItems(object):
+
+    def __init__(self):
+
+        # A Batch is a collection of vertex lists for batched rendering.
+        self.batch = pyglet.graphics.Batch()
+
+        # A mapping from position to the texture of the block at that position.
+        # This defines all the blocks that are currently in the world.
+        self.world = {}
+
+        # Same mapping as `world` but only contains blocks that are shown.
+        self.shown = {}
+
+        # Mapping from position to a pyglet `VertextList` for all shown blocks.
+        self._shown = {}
+
+        # Mapping from sector to a list of positions inside that sector.
+        self.sectors = {}
+
+        # Simple function queue implementation. The queue is populated with
+        # _show_block() and _hide_block() calls
+        self.queue = deque()
+
+        self._initialize()
+
+    def _initialize(self):
+        pass
+
+    def hit_test(self, position, vector, max_distance=8):
+        """ Line of sight search from current position. If a block is
+        intersected it is returned, along with the block previously in the line
+        of sight. If no block is found, return None, None.
+
+        Parameters
+        ----------
+        position : tuple of len 3
+            The (x, y, z) position to check visibility from.
+        vector : tuple of len 3
+            The line of sight vector.
+        max_distance : int
+            How many blocks away to search for a hit.
+
+        """
+        m = 8
+        x, y, z = position
+        dx, dy, dz = vector
+        previous = None
+        for _ in xrange(max_distance * m):
+            key = normalize((x, y, z))
+            if key != previous and key in self.world:
+                return key, previous
+            previous = key
+            x, y, z = x + dx / m, y + dy / m, z + dz / m
+        return None, None
+
+    def exposed(self, position):
+        """ Returns False is given `position` is surrounded on all 6 sides by
+        blocks, True otherwise.
+
+        """
+        x, y, z = position
+        for dx, dy, dz in FACES:
+            if (x + dx, y + dy, z + dz) not in self.world:
+                return True
+        return False
+
+    def add_block(self, position, texture, immediate=True):
+        """ Add a block with the given `texture` and `position` to the world.
+
+        Parameters
+        ----------
+        position : tuple of len 3
+            The (x, y, z) position of the block to add.
+        texture : list of len 3
+            The coordinates of the texture squares. Use `tex_coords()` to
+            generate.
+        immediate : bool
+            Whether or not to draw the block immediately.
+
+        """
+        if position in self.world:
+            self.remove_block(position, immediate)
+        self.world[position] = texture
+        self.sectors.setdefault(sectorize(position), []).append(position)
+        if immediate:
+            if self.exposed(position):
+                self.show_block(position)
+            self.check_neighbors(position)
+
+    def remove_block(self, position, immediate=True):
+        """ Remove the block at the given `position`.
+
+        Parameters
+        ----------
+        position : tuple of len 3
+            The (x, y, z) position of the block to remove.
+        immediate : bool
+            Whether or not to immediately remove block from canvas.
+
+        """
+        del self.world[position]
+        self.sectors[sectorize(position)].remove(position)
+        if immediate:
+            if position in self.shown:
+                self.hide_block(position)
+            self.check_neighbors(position)
+
+    def check_neighbors(self, position):
+        """ Check all blocks surrounding `position` and ensure their visual
+        state is current. This means hiding blocks that are not exposed and
+        ensuring that all exposed blocks are shown. Usually used after a block
+        is added or removed.
+
+        """
+        x, y, z = position
+        for dx, dy, dz in FACES:
+            key = (x + dx, y + dy, z + dz)
+            if key not in self.world:
+                continue
+            if self.exposed(key):
+                if key not in self.shown:
+                    self.show_block(key)
+            else:
+                if key in self.shown:
+                    self.hide_block(key)
+
+    def show_block(self, position, immediate=True):
+        """ Show the block at the given `position`. This method assumes the
+        block has already been added with add_block()
+
+        Parameters
+        ----------
+        position : tuple of len 3
+            The (x, y, z) position of the block to show.
+        immediate : bool
+            Whether or not to show the block immediately.
+
+        """
+        texture = self.world[position].texture_coords
+        group = self.world[position].group
+        self.shown[position] = texture
+        if immediate:
+            self._show_block(position, group, texture)
+        else:
+            self._enqueue(self._show_block, position, group, texture)
+
+    def _show_block(self, position, group, texture):
+        """ Private implementation of the `show_block()` method.
+
+        Parameters
+        ----------
+        position : tuple of len 3
+            The (x, y, z) position of the block to show.
+        texture : list of len 3
+            The coordinates of the texture squares. Use `tex_coords()` to
+            generate.
+
+        """
+        x, y, z = position
+        vertex_data = cube_vertices(x, y, z, 0.1)
+        texture_data = list(texture)
+        # create vertex list
+        # FIXME Maybe `add_indexed()` should be used instead
+        self._shown[position] = self.batch.add(24, GL_QUADS, group,
+            ('v3f/static', vertex_data),
+            ('t2f/static', texture_data))
+
+    def hide_block(self, position, immediate=True):
+        """ Hide the block at the given `position`. Hiding does not remove the
+        block from the world.
+
+        Parameters
+        ----------
+        position : tuple of len 3
+            The (x, y, z) position of the block to hide.
+        immediate : bool
+            Whether or not to immediately remove the block from the canvas.
+
+        """
+        self.shown.pop(position)
+        if immediate:
+            self._hide_block(position)
+        else:
+            self._enqueue(self._hide_block, position)
+
+    def _hide_block(self, position):
+        """ Private implementation of the 'hide_block()` method.
+
+        """
+        self._shown.pop(position).delete()
+
+    def show_sector(self, sector):
+        """ Ensure all blocks in the given sector that should be shown are
+        drawn to the canvas.
+
+        """
+        for position in self.sectors.get(sector, []):
+            if position not in self.shown and self.exposed(position):
+                self.show_block(position, False)
+
+    def hide_sector(self, sector):
+        """ Ensure all blocks in the given sector that should be hidden are
+        removed from the canvas.
+
+        """
+        for position in self.sectors.get(sector, []):
+            if position in self.shown:
+                self.hide_block(position, False)
+
+    def change_sectors(self, before, after):
+        """ Move from sector `before` to sector `after`. A sector is a
+        contiguous x, y sub-region of world. Sectors are used to speed up
+        world rendering.
+
+        """
+        before_set = set()
+        after_set = set()
+        pad = 4
+        for dx in xrange(-pad, pad + 1):
+            for dy in [0]:  # xrange(-pad, pad + 1):
+                for dz in xrange(-pad, pad + 1):
+                    if dx ** 2 + dy ** 2 + dz ** 2 > (pad + 1) ** 2:
+                        continue
+                    if before:
+                        x, y, z = before
+                        before_set.add((x + dx, y + dy, z + dz))
+                    if after:
+                        x, y, z = after
+                        after_set.add((x + dx, y + dy, z + dz))
+        show = after_set - before_set
+        hide = before_set - after_set
+        for sector in show:
+            self.show_sector(sector)
+        for sector in hide:
+            self.hide_sector(sector)
+
+    def _enqueue(self, func, *args):
+        """ Add `func` to the internal queue.
+
+        """
+        self.queue.append((func, args))
+
+    def _dequeue(self):
+        """ Pop the top function from the internal queue and call it.
+
+        """
+        func, args = self.queue.popleft()
+        func(*args)
+
+    def process_queue(self):
+        # Check to see if world item is in range of the player.
+        #pos_rounded = (int(round(WINDOW.position[0])), int(round(WINDOW.position[1])), int(round(WINDOW.position[2])))
+        #print str(pos_rounded)
+
+        #item = False
+        #for k,v in self.world.iteritems():
+        #    if k[0] == pos_rounded[0] and k[2] == pos_rounded[2]:
+        #        item = getInventoryItemBlockFromWorldBlockPosition(k)
+        #        break
+        #item = getInventoryItemBlockFromWorldBlockPosition()
+
+        #if(item != False):
+        #    #config.InventoryItem_MultiTool_use(params, item)
+        #    WINDOW.player.inventory.add(item)
+        #    self.remove_block(WINDOW.position)
+
+        """ Process the entire queue while taking periodic breaks. This allows
+        the game loop to run smoothly. The queue contains calls to
+        _show_block() and _hide_block() so this method should be called if
+        add_block() or remove_block() was called with immediate=False
+
+        """
+        start = time.clock()
+        while self.queue and time.clock() - start < 1.0 / TICKS_PER_SEC:
+            self._dequeue()
+
+    def process_entire_queue(self):
+        """ Process the entire queue with no breaks.
+
+        """
+        while self.queue:
+            self._dequeue()
+
+
+
 class MenuItem(object):
     def __init__(self, window, image_file, pos_x, pos_y):
         self.image_file = image_file
@@ -525,8 +813,6 @@ class UI(object):
         self.window.drawregister.removeAfter(self.test.draw, 1)
         self.itemkeypressed.append(self.test.draw)
 
-        
-
 class DrawRegister(object):
     def __init__(self):
         self.drawregister = []
@@ -566,7 +852,16 @@ class InventoryItem(object):
         pass
     # Drop always behaves in the same way, so it's not abstract.
     def drop(self):
-        pass
+        blk = BLOCKS.get(self.name)
+        if(blk == None):
+            BLOCKS[self.name] = Block(self.ui_texture)
+            BLOCKS[self.name].inventory_item = self
+        vector = WINDOW.get_sight_vector()
+        block, previous = WINDOW.model.hit_test(WINDOW.position, vector)
+        if(previous):
+            WINDOW.world_items.add_block(previous, blk)
+            WINDOW.player.inventory.remove(self)
+            WINDOW.player.selected = WINDOW.player.inventory.findNewSelected()
 
 class InventoryItem_Block(InventoryItem):
     def __init__(self, blocktype, name=False):
@@ -574,6 +869,10 @@ class InventoryItem_Block(InventoryItem):
             name = blocktype
         # Max quantity of a stack of blocks is 64.
         self.worldblock = BLOCKS[blocktype]
+        try:
+            self.inventory_item = BLOCKS[blocktype].inventory_item
+        except:
+            pass
         super(InventoryItem_Block, self).__init__(name, 64, self.worldblock.texture_file)
     def use(self, params):
         # Place the block
@@ -592,6 +891,18 @@ def getInventoryItemBlockFromWorldBlockPosition(worldblockposition):
     try:
         for k,v in blocks.iteritems():
             if(WINDOW.model.world[worldblockposition] == BLOCKS[k]):
+                return blocks[k]
+    except:
+        return False
+    return False
+
+def getInventoryItemBlockFromWorldItemPosition(worlditemposition):
+    blocks = {}
+    for k,v in BLOCKS.iteritems():
+        blocks[k] = InventoryItem_Block(k)
+    try:
+        for k,v in blocks.iteritems():
+            if(WINDOW.world_items.world[worlditemposition] == BLOCKS[k]):
                 return blocks[k]
     except:
         return False
@@ -625,20 +936,35 @@ class Inventory(object):
                 return
         self.inventory.append(item)
     def remove(self, item, qty=1):
-        print "REMOVE:::" + str(item.ui_position)
         for a,i in enumerate(self.inventory):
             if(i != False):
                 if(i.name == item.name):
                     self.window.UI.menu_item_manager.removeItem(a)
                     self.inventory[a] = False
                     return
+    def findNewSelected(self):
+        for k,i in enumerate(self.inventory):
+            if(i != False):
+                return i
+        return False
 
 class Player(object):
     def __init__(self, window):
         #self.inventory = [InventoryItem_Block("BRICK"), InventoryItem_Block("GRASS"), InventoryItem_Block("SAND")]
         self.inventory = Inventory(window)
         self.inventory.add(InventoryItem_MultiTool())
-        self.selected = self.inventory.inventory[0]
+        self.selected = self.inventory.findNewSelected()
+        self.window = window
+    def pickup(self):
+        vector = self.window.get_sight_vector()
+        block, previous = self.window.model.hit_test(self.window.position, vector)
+        try:
+            item = getInventoryItemBlockFromWorldItemPosition(previous).inventory_item
+        except:
+            item = getInventoryItemBlockFromWorldItemPosition(previous)
+        if(item != False):
+            self.window.player.inventory.add(item)
+            self.window.world_items.remove_block(previous)
 
 class Window(pyglet.window.Window):
 
@@ -701,6 +1027,7 @@ class Window(pyglet.window.Window):
 
         self.UI = UI(self)
         self.player = Player(self)
+        self.world_items = WorldItems()
 
     def set_exclusive_mouse(self, exclusive):
         """ If `exclusive` is True, the game will capture the mouse, if False
@@ -777,6 +1104,7 @@ class Window(pyglet.window.Window):
 
         """
         self.model.process_queue()
+        self.world_items.process_queue()
         sector = sectorize(self.position)
         if sector != self.sector:
             self.model.change_sectors(self.sector, sector)
@@ -940,6 +1268,10 @@ class Window(pyglet.window.Window):
             self.strafe[1] -= 1
         elif symbol == key.D:
             self.strafe[1] += 1
+        elif symbol == key.Q:
+            self.player.selected.drop()
+        elif symbol == key.E:
+            self.player.pickup()
         elif symbol == key.SPACE:
             if self.dy == 0:
                 self.dy = JUMP_SPEED
@@ -1037,6 +1369,7 @@ class Window(pyglet.window.Window):
         self.set_3d()
         glColor3d(1, 1, 1)
         self.model.batch.draw()
+        self.world_items.batch.draw()
         self.draw_focused_block()
         self.set_2d()
         self.draw_label()
